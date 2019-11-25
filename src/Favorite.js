9e138cda39c2ccbb87d858612b4d388babc018f9
sachinm78 <sachinm78@gmail.com>
2019-11-25 15:15:20 -0600

    refactored Favroite.js and Menu.js to implement toggler HOC

--- src/Favorite.js
+++ src/Favorite.js
@@ -2,27 +2,15 @@ import React, {Component} from "react"
 import {withToggler} from "./HOCs/withToggler"
 
 class Favorite extends Component {
-    state = {
-        isFavorited: false
-    }
-    
-    toggleFavorite = () => {
-        this.setState(prevState => {
-            return {
-                isFavorited: !prevState.isFavorited
-            }
-        })
-    }
-    
     render() {
         return (
             <div>
                 <h3>Click heart to favorite</h3>
                 <h1>
                     <span 
-                        onClick={this.toggleFavorite}
+                        onClick={this.props.toggle}
                     >
-                        {this.state.isFavorited ? "❤️" : "♡"}
+                        {this.props.on ? "❤️" : "♡"}
                     </span>
                 </h1>
             </div>
--- src/Menu.js
+++ src/Menu.js
@@ -1,24 +1,11 @@
 import React, {Component} from "react"
 import {withToggler} from "./HOCs/withToggler"
-
 class Menu extends Component {
-    state = {
-        show: true
-    }
-    
-    toggleShow = () => {
-        this.setState(prevState => {
-            return {
-                show: !prevState.show
-            }
-        })
-    }
-    
     render() {
         return (
             <div>
-                <button onClick={this.toggleShow}>{this.state.show ? "Hide" : "Show"} Menu </button>
-                <nav style={{display: this.state.show ? "block" : "none"}}>
+                <button onClick={this.props.toggle}>{this.props.on ? "Hide" : "Show"} Menu </button>
+                <nav style={{display: this.props.on ? "block" : "none"}}>
                     <h6>Signed in as Coder123</h6>
                     <a>Your Profile</a>
                     <a>Your Repositories</a>
